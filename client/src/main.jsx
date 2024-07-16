cf0fad3fb51c374fcdbd6db57d11d8f69d39423a
gitongah <mbaabugitonga@gmail.com>
2024-07-16 11:30:05 -0700

    added the login and signup page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
+import Login from './pages/Login/Login.jsx'
+import Signup from './pages/Signup/Signup.jsx';
+import LandingPage from './pages/LandingPage/LandingPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <Login/>
+      },
+      {
+        path: '/signup',
+        element: <Signup/>
+    
+      },{
+        path:'/home',
+        element: <LandingPage/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router={router} />
 )
